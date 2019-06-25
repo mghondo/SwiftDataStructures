cdd4701d5f713f643c0297e4b36cc9a4cec4c953
Morgan Hondros <morganhondros@gmail.com>
2019-06-25 15:04:09 -0400

    Lecture 6. Linear Time

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,11 @@
+// Linear Time
+
+var names = ["Darclee", "Delmar", "Beaufort", "Pete", "GigMobile", "Jon", "Platform", "Eats"]
+
+func printNames(names : [String]) {
+    for name in names {
+        print(name)
+    }
+}
+
+printNames(names: names)
