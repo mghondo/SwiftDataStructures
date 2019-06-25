7cc585dfbc99058eeaeb9279932d8b4c8d7fb43e
Morgan Hondros <morganhondros@gmail.com>
2019-06-25 18:06:44 -0400

    End of Lecture 11. Implementing a node class. Cool Stuff.

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,27 +1,29 @@
-// Linear Time
+import UIKit
 
-//var names = ["Darclee", "Delmar", "Beaufort", "Pete", "GigMobile", "Jon", "Platform", "Eats"]
-
-func containsUsingBinarySearch(_ value :Int) -> Bool {
+class Node<Value> {
+    var value : Value
+    var next : Node?
     
-    var lowerBound = 0
-    var upperBound = self.count - 1
-    var middle = 0
-    var found = false
-    
-    while(lowerBound <= upperBound) {
-        
-        middle = (lowerBound + upperBound) / 2
-        
-        if( numbers[middle] == value) {
-            found = true
-            break
-        } else if (numbers[middle] < value ) {
-            lowerBound = middle + 1
-        } else {
-            upperBound = middle - 1
-        }
+    init(value : Value, next :Node? = nil) {
+        self.value = value
+        self.next = next
     }
-    return found
-    
 }
+
+extension Node :CustomStringConvertible {
+    var description :String {
+        guard let next = next else {
+            return "\(value)"
+        }
+        return "\(value) -> " + String(describing: next) + " "
+    }
+}
+
+let node1 = Node(value: 1)
+let node2 = Node(value: 2)
+let node3 = Node(value: 3)
+
+node1.next = node2
+node2.next = node3
+
+print(node1)
