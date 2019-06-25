ca7031735660830abf1db644f4760947b3863727
Morgan Hondros <morganhondros@gmail.com>
2019-06-25 16:40:04 -0400

    Lecture 8. Logrithmic Time

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,13 +1,27 @@
 // Linear Time
 
-var names = ["Darclee", "Delmar", "Beaufort", "Pete", "GigMobile", "Jon", "Platform", "Eats"]
+//var names = ["Darclee", "Delmar", "Beaufort", "Pete", "GigMobile", "Jon", "Platform", "Eats"]
 
-func printNames(names : [String]) {
-    for _ in names {
-        for name in names {
-            print(name)
+func containsUsingBinarySearch(_ value :Int) -> Bool {
+    
+    var lowerBound = 0
+    var upperBound = self.count - 1
+    var middle = 0
+    var found = false
+    
+    while(lowerBound <= upperBound) {
+        
+        middle = (lowerBound + upperBound) / 2
+        
+        if( numbers[middle] == value) {
+            found = true
+            break
+        } else if (numbers[middle] < value ) {
+            lowerBound = middle + 1
+        } else {
+            upperBound = middle - 1
         }
     }
+    return found
+    
 }
-
-printNames(names: names)
